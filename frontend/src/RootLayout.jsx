47f24bc2d02d6caac3e94c483527aed84b4a2164
alfardan <alfardanarfin500@gmail.com>
2025-05-28 14:21:58 +0530

    feat: add Redux for state management and implement routing with new components

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from "@tanstack/react-router";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/NavBar";
 
 const RootLayout = () => {
   return (
     <>
-    <HomePage />
+    <Navbar />
+    <Outlet />
     </>
   )
 }
--- frontend/src/routing/routeTree.js
+++ frontend/src/routing/routeTree.js
@@ -1,11 +1,15 @@
 import { createRootRoute } from "@tanstack/react-router"
-
 import RootLayout from "../RootLayout"
+import { dashboardRoute } from "./dashboard"
+import { authRoute } from "./auth.route"
+import { homePageRoute } from "./homepage"
 
 export const rootRoute = createRootRoute({
     component: RootLayout
 })
 
-export const routeTree =rootRoute.addChildren([
-   
+export const routeTree=rootRoute.addChildren([
+    homePageRoute,authRoute,dashboardRoute
 ])
+
+
