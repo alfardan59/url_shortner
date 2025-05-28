fe9fb879716e8c5f2591824660a344b0fc48464f
alfardan <alfardanarfin500@gmail.com>
2025-05-28 15:14:37 +0530

    feat: add logout and current user retrieval functionality

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -6,11 +6,21 @@ export const loginUser = async (password,email) =>{
 }
 
 export const registerUser = async (name,password,email) =>{
-    const {data} = await axiosInstance.post("/api/auth/login",{name,email,password})
+    const {data} = await axiosInstance.post("/api/auth/register",{name,email,password})
     return data
 }
 
 export const logoutUser = async () =>{
     const {data} = await axiosInstance.get("/api/auth/logout")
+    return data
+}
+
+export const getCurrentUser = async () =>{
+    const {data} = await axiosInstance.get("/api/auth/me")
+    return data
+}
+
+export const getAllUserUrls = async () =>{
+    const {data} = await axiosInstance.post("/api/user/urls")
     return data
 }
