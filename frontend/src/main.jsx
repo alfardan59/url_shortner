b36f8ec1f50bfb632f066f5848bc55c28e108579
alfardan <alfardanarfin500@gmail.com>
2025-05-28 15:02:07 +0530

    feat: implement Redux for authentication and protect dashboard route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,20 @@
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
-import {RouteProvider,createRouter} from '@tanstack/react-router';
-import { routeTree } from './routing/routeTree';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouteProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routing/routeTree";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
-const queryClient=new QueryClient()
+const queryClient = new QueryClient();
 const router = createRouter({
   routeTree,
 });
 
-createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <RouteProvider router={router} />
-  </QueryClientProvider>
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RouteProvider router={router} />
+    </QueryClientProvider>
+  </Provider>
 );
---
+++ frontend/src/utils/helper.js
@@ -0,0 +1,9 @@
+import store from "../store/store"
+import { redirect } from "@tanstack/react-router";
+
+export const checkAuth=()=>{
+    const auth=store.getState().auth;
+    if(!auth.isAuthenticated){
+        throw redirect({to:"/auth"})
+    }
+}
